0011e2a12025f139ee02025dc76f299712f0ec48
PAULO HENRIQUE DE ANDRADE JUNIOR <21193420@CFP127ALUNO.LOCAL>
2021-08-31 17:00:52 -0300

    Adicional

--- js/lampada.js
+++ js/lampada.js
@@ -1,6 +1,7 @@
 "use strict"
-
 const lampada = document.getElementById("lampada")
+let idLigar
+let idDesligar
 
 //funções
 function ligarLampada(){
@@ -20,10 +21,29 @@ function lampadaInteira(){
     //utilizando includes para identificar se a lampada está inteira
     return lampada.src.includes("ligada")
 }
+function piscarLampada(){
+    const botaoPiscar = document.getElementById("piscar")
+    if(botaoPiscar.textContent == "piscar"){
+        setInterval(ligarLampada, 500)
+        setInterval(desligarLampada, 1000)
+    }
+    else{
+        parar()
+        botaoPiscar.textContent = "piscar"
+    }
+    
+}
+function parar(){
+    
+}
+ 
+     
 
 //eventos
 document.getElementById("ligar").addEventListener("click", ligarLampada)
 document.getElementById("desligar").addEventListener("click", desligarLampada)
+document.getElementById("piscar").addEventListener("click", piscarLampada)
+document.getElementById("parar").addEventListener("click", parar)
 lampada.addEventListener("dblclick", quebrarLampada)
 lampada.addEventListener("mouseover", ligarLampada)
 lampada.addEventListener("mouseout", desligarLampada)
